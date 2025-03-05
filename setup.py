588cc3464977d0435cd197c370296f5b5006a647
Luis-Florez-Escobar <Luis.florez37@est.iudigital.edu.co>
2025-03-05 14:13:54 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="actividad_1",
+    version="0.0.1",
+    author="Luis Carlos Florez Escobar",
+    author_email="Luis.florez37@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests"
+    ]
+) 
---
+++ src/pad/actividad_1.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+class Actividad1:
+    def __init__(self):
+        self.ruta_static = "src/pad/static/"
+
+    def leer_api(self,url="",params={}):
+        if len(parametros)==0:
+            url = "{}/{}/{}/".format(url,params["coin"],params["method"])
+        else:
+            url = url
+            
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            print(error)
+            return {}
+    
+    def escribir_json(self,datos={},nombre_archivo="ingestion"):
+        with open("{}json/{}.json".format(self.ruta_static,nombre_archivo), "w") as archivo:
+            json.dump(datos,archivo)
+        
+
+    
+act = Actividad1()
+parametros = {"coin":"BTC","method":"ticker"}
+url= "https://api.disneyapi.dev/character"
+datos = act.leer_api(url=url)
+if len(datos)>0:
+    print(json.dumps(datos,indent=4))
+else:
+    print("no se obtubo la consulta de api")
+act.escribir_json(datos=datos,nombre_archivo="ingestion")
